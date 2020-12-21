f85a469906c9d7522e2d48370cadff85242ec8ac
za-zhaoyun001 <zhaoyun001@zhongan.io>
2020-12-21 14:53:26 +0800

    feat: DebugReact

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {React, ReactDOM} from "./CONST";
+import App from "./App";
+import "./index.css";
+import jsx from "./pages/ExamplePage";
+// import DiffPage from "./pages/DiffPage";
+
+// ReactDOM.render(<DiffPage />, document.getElementById("root"));
+ReactDOM.render(jsx, document.getElementById("root"));
+
+
+console.log("version-sy-log", React.version); //sy-log
